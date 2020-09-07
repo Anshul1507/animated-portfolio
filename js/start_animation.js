6442ac4d62dbbc0c999e7c23c75047bb5804596a
Anshul1507 <ag1507anshul@gmail.com>
2020-09-07 09:54:10 +0530

    #2 Added - Text Switcher for sub-title

--- js/start_animation.js
+++
@@ -1,9 +0,0 @@
-// Used for set the animation
-document.addEventListener('DOMContentLoaded', function() {
-    particleground(document.getElementById('animations'), {
-        dotColor: '#5cbdaa',
-        lineColor: '#5cbdaa'
-    });
-    var intro = document.getElementById('main_body');
-    intro.style.marginTop = -intro.offsetHeight / 2 + 'px';
-}, false);
